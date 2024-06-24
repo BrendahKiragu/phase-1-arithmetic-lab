906bf95bddae9b16cdc25bfc7a2ab5c1bf45a35e
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-24 23:02:53 +0300

    creates a mod

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const num1 = 8;
-const num2 = 8;
+const num1 = 31;
+const num2 = 2;
 const multiply = num1 * num2;
 //console.log(multiply)
+
+const random = Math.floor(Math.random()*10);
+//console.log(random)
+
+const num3 =8;
+const num4 =2;
+const mod = num3 /num4
+//console.log(mod)
