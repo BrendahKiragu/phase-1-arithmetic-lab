36d1f7f56d5496f744ee6b444ffd414cf9630ccf
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-25 11:41:50 +0300

    completes the arithmetic tests

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const num1 = 31;
-const num2 = 2;
+const num1 =1;
+const num2 =62;
 const multiply = num1 * num2;
-//console.log(multiply)
+multiply;
 
-const random = Math.floor(Math.random()*10);
+const random = Math.ceil(Math.random()*100) 
+random;
 //console.log(random)
 
-const num3 =8;
-const num4 =2;
-const mod = num3 /num4
-//console.log(mod)
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max (20, 13, 5, 8, 10)
+max;
